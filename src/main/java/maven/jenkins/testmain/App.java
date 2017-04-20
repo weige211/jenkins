34614431f4ae669eb4ba806b066d34865c53b409
Your Name <you@example.com>
2017-04-20 11:03:53 +0800

    add maven

---
+++ src/main/java/maven/jenkins/testmain/App.java
@@ -0,0 +1,13 @@
+package maven.jenkins.testmain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!I am weige" );
+    }
+}
